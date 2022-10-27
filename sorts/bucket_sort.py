184a6f1a9f38e14a829b3a2e3a96c53096f88532
patraabinash29 <63745266+patraabinash29@users.noreply.github.com>
2022-10-27 18:28:29 +0530

    added print

--- sorts/bucket_sort.py
+++ sorts/bucket_sort.py
@@ -66,3 +66,4 @@ if __name__ == "__main__":
     testmod()
     assert bucket_sort([4, 5, 3, 2, 1]) == [1, 2, 3, 4, 5]
     assert bucket_sort([0, 1, -10, 15, 2, -2]) == [-10, -2, 0, 1, 2, 15]
+print('sorting done')
